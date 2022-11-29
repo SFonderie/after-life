58cdadd734457217166a534bf181d13efbb2fd99
SFonderie <sydney3449@gmail.com>
2022-11-28 23:29:33 -0800

    Added Pickups!
    Not the collectable kind, but the inspectable kind!

--- Afterlife/Assets/Scripts/Player/IPlayerInteractable.cs
+++ Afterlife/Assets/Scripts/Player/IPlayerInteractable.cs
@@ -1,9 +1,17 @@
-using UnityEngine;
-
+/// <summary>
+/// Allows a script to react to player interactions.
+/// </summary>
 public interface IPlayerListener
 {
 	/// <summary>
+	/// Invoked once each frame that a player hovers over this object.
+	/// </summary>
+	/// <param name="context">Player context from update.</param>
+	public void OnHover(PlayerContext context);
+
+	/// <summary>
 	/// Invoked each time that the player interacts with this object.
 	/// </summary>
-	public void OnInteraction(GameObject player);
+	/// <param name="context">Player context from update.</param>
+	public void OnInteraction(PlayerContext context);
 }
--- Afterlife/Assets/Scripts/Player/PlayerInteraction.cs
+++ Afterlife/Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// Implements player look-based interaction.
+/// </summary>
 public class PlayerInteraction : PlayerDelegate
 {
 	/// <summary>
@@ -10,16 +13,37 @@ public class PlayerInteraction : PlayerDelegate
 	private Transform PlayerLook = null;
 
 	/// <summary>
+	/// Player inspection position.
+	/// </summary>
+	[SerializeField, Tooltip("Player inspection position.")]
+	private Transform PlayerInspect = null;
+
+	/// <summary>
 	/// Interaction sphere cast length.
 	/// </summary>
 	[Min(0), SerializeField, Tooltip("Interaction sphere cast length.")]
-	private float InteractRange = 10f;
+	private float InteractRange = 8f;
 
 	/// <summary>
 	/// Interaction sphere cast radius.
 	/// </summary>
 	[Min(0), SerializeField, Tooltip("Interaction sphere cast radius.")]
-	private float InteractRadius = 1f;
+	private float InteractRadius = 0.5f;
+
+	/// <summary>
+	/// Current tracked player listener.
+	/// </summary>
+	private IPlayerListener Listener = null;
+
+	/// <summary>
+	/// Should an interaction occur this frame?
+	/// </summary>
+	private bool DoInteract = false;
+
+	/// <summary>
+	/// Should interaction be ignored?
+	/// </summary>
+	private bool BlockInteract = false;
 
 	void Start()
 	{
@@ -27,15 +51,63 @@ public class PlayerInteraction : PlayerDelegate
 		{
 			Debug.LogError("[PLAYER INTERACTION] - NO CAMERA TRANSFORM LINKED; INTERACTION DISABLED.");
 		}
+
+		if (!PlayerInspect)
+		{
+			Debug.LogError("[PLAYER INTERACTION] - NO INSPECTION TRANSFORM LINKED; INTERACTION DISABLED.");
+		}
 	}
 
 	public override void HandleInput(InputAction.CallbackContext context)
 	{
+		if (context.action.name.Equals("Interact"))
+		{
+			DoInteract = context.performed && !BlockInteract;
+			BlockInteract = BlockInteract && !(BlockInteract && context.canceled);
+		}
+	}
 
+	void FixedUpdate()
+	{
+		if (!PlayerLook || !PlayerInspect)
+		{
+			return;
+		}
+
+		foreach (RaycastHit hit in Physics.SphereCastAll(PlayerLook.position, InteractRadius, PlayerLook.forward, InteractRange))
+		{
+			IPlayerListener component = hit.transform.GetComponent<IPlayerListener>();
+
+			if (component != null)
+			{
+				Listener = component;
+				return;
+			}
+		}
+
+		Listener = null;
 	}
 
 	public override void UpdateDelegate(PlayerContext context)
 	{
-		
+		if (!PlayerLook || !PlayerInspect)
+		{
+			return;
+		}
+
+		context.InspectTransform = PlayerInspect;
+
+		if (Listener != null)
+		{
+			Listener.OnHover(context);
+
+			if (DoInteract && !BlockInteract)
+			{
+				Listener.OnInteraction(context);
+
+				DoInteract = false;
+				BlockInteract = true;
+			}
+		}
 	}
 }

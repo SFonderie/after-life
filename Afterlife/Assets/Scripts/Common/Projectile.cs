9be5d3f69abf846b604af0ef1c42ec67ade54285
SFonderie <sydney3449@gmail.com>
2022-12-04 13:20:26 -0800

    Minor Changes
    Also added an import of the first set of asset packages.

--- Afterlife/Assets/Scripts/Common/Projectile.cs
+++ Afterlife/Assets/Scripts/Common/Projectile.cs
@@ -2,18 +2,42 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+	/// <summary>
+	/// Projectile physics handler.
+	/// </summary>
 	[SerializeField, Tooltip("Projectile physics handler.")]
 	private Rigidbody ProjectilePhysics = null;
+
+	/// <summary>
+	/// Projectile damage amount.
+	/// </summary>
 	private float Damage = 0;
+
+	/// <summary>
+	/// Projectile life time.
+	/// </summary>
+	private float Lifetime = 0;
 
 	public void OnSpawn(Vector3 velocity, float damage)
 	{
 		ProjectilePhysics.velocity = velocity;
 		Damage = damage;
+		Lifetime = 0;
 	}
 
-	public void OnCollisionEnter(Collision collision)
+	void Update()
 	{
+		Lifetime += Time.deltaTime;
+
+		if (Lifetime > 10)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		collision.gameObject.SendMessageUpwards("TakeDamage", Damage, SendMessageOptions.DontRequireReceiver);
 		Destroy(gameObject);
 	}
 }
